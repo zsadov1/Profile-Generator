0e6c52f07dcf6585e057e5bb97d4b93e1d9883d0
zsadov1 <zsadov1@gmail.com>
2020-01-23 16:17:42 -0500

    Generating html and pdf

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
-const axios = require("axios");
-const inquirer = require("inquirer");
-const puppeteer = require('puppeteer');
 const util = require('util');
+const inquirer = require("inquirer");
+const axios = require("axios");
+const puppeteer = require('puppeteer');
+
+
 const asyncFileWrite = util.promisify(fs.writeFile);
 
 
@@ -17,7 +19,7 @@ function userPrompt()  {
         },
         {
             type: "list",
-            name: "username",
+            name: "color",
             message: "What is your favorite color below?",
             choices: [
                 "Yellow",
@@ -31,6 +33,191 @@ function userPrompt()  {
     ]);
 }
 
-userPrompt()
+
+function htmlProfile (userData, gitData, starsData) {
+    // Generating user github data 
+    return `
+    
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>${gitData.name}</title>
+    <link re="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"/>
+    <style> 
+    
+
+    body {
+        background-color: ${userData.color};
+        color: #B1B1B1;
+      }
+      
+      h3 {
+        color: #333;
+      }
+      
+      .header-position {
+        position: relative;
+        top: 60px;
+        border-radius: 6px;
+        padding: 20px;
+        padding-top: 0;
+      }
+      
+      .header-position img {
+        position: relative;
+        top: -40px;
+        border-radius: 50%;
+        border: 9px solid #fff;
+      }
+      
+      
+      .data-card {
+        margin: 30px;
+        padding: 20px;
+        border-radius: 6px;
+      }
+          
+    
+    
+    </style>
 
 
+    <body>
+
+    <header>
+        <div class="container">
+            <div class='jumbotron'>
+                <img src="${gitData.data.avatar_url}"/>
+                <h1 class="jumbo">${gitData.data.name}</h1>
+                <p class="header-links d-flex justify-content-center flex-wrap"> 
+                <a href="https://www.google.com/maps/place/${gitData.data.location}">${gitData.data.location}</a>
+                <a href="${gitData.data.html_url}">GitHub Profile</a>
+                </p>
+            </div>
+        </div>
+    </header>
+
+<!-- Container below contains github repo stats -->
+
+    <div class="row">
+        <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+            <div class="card h-100">
+            <div class="card-body">
+                <h4 class="card-title">
+                </h4>
+                <p>${gitData.data.bio}</p>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+            <div class="card h-100">
+            <div class="card-body">
+                <h4 class="card-title">
+                <a  >Public Repos:</a >
+                </h4>
+                <p>${gitData.data.public_repos}</p>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+            <div class="card h-100">
+            <div class="card-body">
+                <h4 class="card-title">
+                <a>Followers</a>
+                </h4>
+                <p>${gitData.data.followers}</p>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+            <div class="card h-100">
+            <div class="card-body">
+                <h4 class="card-title">
+                <a>Following:</a>
+                </h4>
+                <p>${gitData.data.following}</p>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+            <div class="card h-100">
+            <div class="card-body">
+                <h4 class="card-title">
+                <a>Stars</a>
+                </h4>
+                <p>${gitData.data.length}</p>
+            </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                <h4>${gitData.data.bio}</h4>
+            </div>
+        </div>
+
+
+</body>
+</html>
+`;
+}
+
+async function init() {
+
+    try {
+        const userData = await userPrompt();
+        const gitData = await axios.get(
+            `https://api.github.com/users/${userData.username}`
+        );
+
+        const starsData = await axios.get(
+            // `https://api.github.com/users/${userData.username}/starred`
+            `https://api.github.com/users/${userData.username}/starred`
+        );
+
+        const html = htmlProfile(userData, gitData, starsData);
+
+        await asyncFileWrite(`profile_${userData.username}.html` , html);
+        console.log(`Successfully wrote profile_${userData.username}.html`);
+
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        await page.setContent(html);
+        await page.emulateMediaFeatures("screen");
+    
+        await page.pdf({
+        path: `profile_${userData.username}.pdf`,
+        format: "A4",
+        printBackground : true
+    });
+
+    console.log(`Successfully wrote profile_${userData.username}.pdf`);
+    await browser.close();
+    process.exit();
+ } catch (err) {
+    console.log(err);
+    }
+}
+
+init();
